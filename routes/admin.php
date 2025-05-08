28e8ee935638adbb201d45049ab8e1177d08615a
dayvison06 <dayvison.rocha@hotmail.com>
2025-05-08 20:19:47 -0300

    FIX: middleware do laravel não estava funcionando após o método group

--- routes/admin.php
+++ routes/admin.php
@@ -3,14 +3,13 @@
 use App\Http\Controllers\Admin\AdminController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('admin')->group(function () {
+Route::prefix('admin')->middleware(['auth','admin'])->group(function () {
     Route::get('/', [AdminController::class, 'index'])->name('admin.dashboard');
     Route::get('/users', [AdminController::class, 'users'])->name('admin.users');
     Route::get('/teams', [AdminController::class, 'teams'])->name('admin.teams');
     Route::get('/roles', [AdminController::class, 'roles'])->name('admin.roles');
     Route::get('/permissions', [AdminController::class, 'permissions'])->name('admin.permissions');
-
-})->middleware(['auth','admin']);
+});
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
