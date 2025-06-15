6a1e90c7c17be58d774a09b2a6eeef6c5a89ecf3
dayvison06 <dayvison.rocha@hotmail.com>
2025-06-15 12:10:53 -0300

    WIP: tradução da tela de configurações

--- resources/js/layouts/settings/Layout.vue
+++ resources/js/layouts/settings/Layout.vue
@@ -27,7 +27,7 @@ const currentPath = page.props.ziggy?.location ? new URL(page.props.ziggy.locati
 
 <template>
     <div class="px-4 py-6">
-        <Heading title="Settings" description="Manage your profile and account settings" />
+        <Heading title="Configurações" description="Gerencie seu perfil e as configurações da conta" />
 
         <div class="flex flex-col space-y-8 md:space-y-0 lg:flex-row lg:space-x-12 lg:space-y-0">
             <aside class="w-full max-w-xl lg:w-48">
