7543852803af4a898453ce5f230218ccbfbc56b2
dayvison06 <dayvison.rocha@hotmail.com>
2025-07-01 00:59:32 -0300

    REMOVE: lógica duplicada

--- resources/js/layouts/AppLayout.vue
+++ resources/js/layouts/AppLayout.vue
@@ -1,10 +1,8 @@
 <script setup lang="ts">
 import AppLayout from '@/layouts/app/AppSidebarLayout.vue';
 import type { BreadcrumbItemType } from '@/types';
-import { usePage } from '@inertiajs/vue3';
 import Toaster from '@/components/ui/sonner/Sonner.vue'
-import { toast } from 'vue-sonner'
-import { onMounted } from 'vue';
+
 interface Props {
     breadcrumbs?: BreadcrumbItemType[];
 }
@@ -13,24 +11,7 @@ withDefaults(defineProps<Props>(), {
     breadcrumbs: () => [],
 });
 
-const page = usePage();
-const unauthorized = page.props.session.message.unauthorized;
 
-onMounted(() => {
-    if (unauthorized) {
-        toast('Acesso negado', {
-            type: 'error',
-            duration: 10000,
-            description: unauthorized,
-            position: 'top-center',
-            classes: {
-                title: 'text-base font-semibold',
-                icon: 'text-red-500'
-            },
-            closeButton: true,
-        });
-    }
-});
 </script>
 
 <template>
