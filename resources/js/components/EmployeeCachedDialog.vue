2b9fc53672988e3c71f191b0e2b9054b11be9658
dayvison06 <dayvison.rocha@hotmail.com>
2025-11-02 16:39:11 -0300

    feat: aprimoramento da interface de funcionários

--- resources/js/components/EmployeeCachedDialog.vue
+++ resources/js/components/EmployeeCachedDialog.vue
@@ -31,7 +31,7 @@ const emit = defineEmits<{
         <DialogContent class="sm:max-w-[425px]">
             <DialogHeader>
                 <DialogTitle>Cadastro de funcionário em andamento</DialogTitle>
-                <span class="text-base text-primary">{{props.cachedEmployeeData.name}}</span>
+                <span class="text-base text-primary">Nome: <small class="text-gray-500 text-base">{{props.cachedEmployeeData.name}}</small></span>
                 <DialogDescription>
                     Deseja continuar a edição ou iniciar um novo cadastro?
                 </DialogDescription>
