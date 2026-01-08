934799c6b26cdaf26f606eaffdb21dc22aceb901
dayvison06 <dayvison.rocha@hotmail.com>
2026-01-07 21:57:33 -0300

    style(ui): ajustes visuais em Input e Layout

--- resources/js/layouts/app/AppSidebarLayout.vue
+++ resources/js/layouts/app/AppSidebarLayout.vue
@@ -22,6 +22,7 @@ withDefaults(defineProps<Props>(), {
             <Toaster rich-colors />
             <AppSidebarHeader :breadcrumbs="breadcrumbs" />
             <div class="mx-auto w-full max-w-screen-2xl mt-8 md:px-6 lg:px-8">
+
                 <slot />
             </div>
         </AppContent>
